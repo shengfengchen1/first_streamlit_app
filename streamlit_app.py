1c2bc7ab9a0a6cbaef00f120982069da431b1a30
shengfengchen1 <100163742+shengfengchen1@users.noreply.github.com>
2023-10-25 14:31:46 -0600

    Update streamlit_app.py
    add Emojis

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,11 @@ import streamlit as st
 st.title('My Parents New Healthy Diner')
 st.write("Hello *world!*")
 st.text("Hello using st.text")
+
 st.header('Breakfast Menu')
-st.text('Omega 3 & Blueberry oatmeal')
-st.text('Kale, Spinach & Rocket Smoothie')
-st.text('Hard-Boiled Free-Range Egg')
+st.text('🥣 Omega 3 & Blueberry oatmeal')
+st.text('🥗 Kale, Spinach & Rocket Smoothie')
+st.text('🐔 Hard-Boiled Free-Range Egg')
+st.text('🥑🍞 Avocado Toast')
+
+st.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
