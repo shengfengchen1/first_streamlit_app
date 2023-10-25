4db42f09888cb85201165c939e54337d8f869bdb
shengfengchen1 <100163742+shengfengchen1@users.noreply.github.com>
2023-10-25 14:26:10 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,3 +3,7 @@ import streamlit as st
 st.title('My Parents New Healthy Diner')
 st.write("Hello *world!*")
 st.text("Hello using st.text")
+st.header('Breakfast Menu')
+st.text('Omega 3 & Blueberry oatmeal')
+st.text('Kale, Spinach & Rocket Smoothie')
+st.text('Hard-Boiled Free-Range Egg')
