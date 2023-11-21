531b07cb08b3b02e05362a06f0c792f75fb68a5b
shengfengchen1 <100163742+shengfengchen1@users.noreply.github.com>
2023-11-21 16:16:09 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -28,17 +28,22 @@ st.dataframe(fruits_to_show)
 #New section to display fruityvice api response
 ###########################################################################################
 st.header('Fruityvice Fruit Advice!')
-fruit_choice = st.text_input('What fruit would you like information about?','Kiwi')
-st.write('The user entered ',fruit_choice)
-
-
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
+try:
+  fruit_choice = st.text_input('What fruit would you like information about?','Kiwi')
+  if not fruit_choice:
+    st.error("Please select a fruit to get information.")
+  else:
+    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
+    # take the json version of the response and normalize it
+    fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
+    # output it the screen as a table
+    st.dataframe(fruityvice_normalized)
+except URLError as e:
+  st.error()
+  
+# st.write('The user entered ',fruit_choice)
 # st.text(fruityvice_response.json())
 
-# take the json version of the response and normalize it
-fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
-# output it the screen as a table
-st.dataframe(fruityvice_normalized)
 
 ##############################################################################################
 # my_cnx = snowflake.connector.connect(**st.secrets["snowflake"])
